baabac618e525d7c85783de4eaaab3761573e1bb
asma05-Nhs <hasninourasma@gmail.com>
2025-04-11 17:11:28 +0200

    Update StockUI.java

--- StockUI.java
+++ StockUI.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
-
+// edit product (add,edit , delete )
 public class StockUI {
 
     private DefaultTableModel model;
